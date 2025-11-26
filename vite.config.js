6c07f284b1fe09aedf52b5dabb1459575bf0ad65
dmdshubham <dmdshubham@gmail.com>
2025-11-26 23:37:00 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['health.familyraksha.com'],  // ✅ Add this line
+    port: 3004,
+    host: '0.0.0.0',
+  },
 })
